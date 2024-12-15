1c39369d64ebcc758514f805e93d38f0632d2020
maryammohamed00 <162592773+maryammohamed00@users.noreply.github.com>
2024-12-15 20:21:54 +0200

    final project

---
+++ Car Rental/wwwroot/js/Booking.js
@@ -0,0 +1,35 @@
+﻿function calculatePrice() {
+    const carSelect = document.getElementById("carSelection");
+    const startDate = document.getElementById("startDate");
+    const endDate = document.getElementById("endDate");
+
+
+    if (carSelect.selectedIndex === -1 || !startDate.value || !endDate.value) {
+        document.getElementById("totalPrice").value = "";
+
+        return;
+    }
+
+
+    const carPrice = parseFloat(carSelect.options[carSelect.selectedIndex].getAttribute("data-price").replace(',', ''));
+
+
+    const start = new Date(startDate.value);
+    const end = new Date(endDate.value);
+
+
+    const timeDiff = end - start;
+    const days = timeDiff / (1000 * 3600 * 24);
+
+
+    if (days < 1) {
+        document.getElementById("totalPrice").value = "Invalid dates";
+
+        return;
+    }
+
+
+    const totalPrice = carPrice * days;
+    document.getElementById("totalPrice").value = totalPrice.toFixed(2) + " EGP"; // Change the currency symbol to EGP
+
+}
---
+++ Car Rental/wwwroot/js/cars.js
@@ -0,0 +1,40 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const filterPrice = document.getElementById('price-range');
+    const filterOrigin = document.getElementById('origin-select');
+    const carItems = document.querySelectorAll('.car-item');
+
+    function filterCars() {
+
+        const selectedPrice = filterPrice.value;
+        const selectedOrigin = filterOrigin.value;
+
+        carItems.forEach(function (carItem) {
+
+            const carPrice = carItem.getAttribute('data-price');
+            const carOrigin = carItem.getAttribute('data-origin');
+
+
+
+            const matchesPrice = selectedPrice === 'all' || carPrice === selectedPrice;
+            const matchesOrigin = selectedOrigin === 'all' || carOrigin === selectedOrigin;
+
+
+            if (matchesPrice && matchesOrigin) {
+                carItem.style.display = 'block';
+            }
+            else {
+                carItem.style.display = 'none';
+            }
+        }
+        );
+    }
+
+
+
+    filterPrice.addEventListener('change', filterCars);
+    filterOrigin.addEventListener('change', filterCars);
+
+
+    filterCars();
+}
+);
